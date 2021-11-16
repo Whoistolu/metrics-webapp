a631c7a73bf4850fa214cd1a5648cce9f09911f6
Whoistolu <digittolu25@gmail.com>
2021-11-16 22:08:48 +0100

    Set up the store

---
+++ src/redux/ConfigureStore.js
@@ -0,0 +1,18 @@
+import {
+  createStore,
+  combineReducers,
+  applyMiddleware,
+} from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
+const reducer = combineReducers({
+  continentReducer,
+});
+const store = createStore(
+  reducer,
+
+  applyMiddleware(logger, thunk),
+);
+
+export default store;
