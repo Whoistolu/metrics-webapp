0678bbca26f0c5bb6730b4a0fc890614d5324ba5
Whoistolu <digittolu25@gmail.com>
2021-11-18 22:32:13 +0100

    Style the homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,22 +4,27 @@ import { useSelector } from "react-redux";
 
 const HomePage = () => {
   const europeanCountries = useSelector(state => state.continentReducer)
-  if (!europeanCountries) return <h1>loading</h1>
+  // if (!europeanCountries) return <h1>loading</h1>
   return (
     <>
+    <nav className="nav-bar">
+      <div className="navbar-container"> 
+        <button>{'<'}</button>
+      </div>
+    </nav>
+    <div className="main-container">
+    <section className="country-container">
       {
       europeanCountries.map((item) => (
-        <div>
-          <h2>{item.country}</h2>
-          <h2>case: {item.description1}</h2>
+        <div className="container">
+          <h2 className="name">{item.country}</h2>
           <img src={item.countryFlag} alt="country flag" />
-          <h2>active: {item.description2}</h2>
-          <h2>test: {item.description3}</h2>
-          <h2>population: {item.description4}</h2>
           <h2>{item.id}</h2>
         </div>
       ))
       }
+       </section>
+    </div>
     </>
   );
 };
